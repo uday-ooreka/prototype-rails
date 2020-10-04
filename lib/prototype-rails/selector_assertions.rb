5fa4d97d62a0d65b2081e5eb1cb785cb5c96568c
uday-ooreka <72012580+uday-ooreka@users.noreply.github.com>
2020-10-04 11:39:24 +0530

    Update selector_assertions.rb

--- lib/prototype-rails/selector_assertions.rb
+++ lib/prototype-rails/selector_assertions.rb
@@ -1,5 +1,4 @@
 require 'active_support/core_ext/module/aliasing'
-require 'action_view/vendor/html-scanner'
 require 'action_dispatch/testing/assertions'
 require 'action_dispatch/testing/assertions/selector'
 
